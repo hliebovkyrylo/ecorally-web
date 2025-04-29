5538907bc017cf316e9c54b75b7338d200365fe6
hliebovkyrylo <kirilglebov06@gmail.com>
2025-04-29 11:21:11 +0300

    feat: the layout of the page for viewing all events has been done

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,22 +1,21 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center cursor-pointer gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        default:
-          "bg-blue-400 text-accent shadow-xs hover:bg-blue-500",
+        default: "bg-blue-400 text-accent shadow-xs hover:bg-blue-500",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        ghost: "hover:bg-secondary text-accent",
         outline:
           "border bg-secondary text-accent shadow-xs hover:bg-secondary-foreground dark:bg-input/30 border-secondary-foreground dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-accent shadow-xs hover:bg-secondary/80",
+        secondary: "bg-secondary text-accent shadow-xs hover:bg-secondary/80",
       },
       size: {
         default: "h-12 px-4 py-2 has-[>svg]:px-3",
@@ -30,7 +29,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
@@ -40,9 +39,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -50,7 +49,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
